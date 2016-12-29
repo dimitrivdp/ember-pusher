81f85009fbd5f8318efce29f9992cb7d61dfc2fc
Dimitri Van de Putte <dimitri@vandeputte.be>
2016-12-29 21:28:46 +0100

    Resolve client-events bug by passing in the pusherInstance
    See issue 43

--- addon/mixins/client-events.js
+++ addon/mixins/client-events.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   // Fire an event programmatically. All Events must unfortunately use
   // the client-<eventname> format for client events (a pusher restriction).
-  pusherTrigger(channelName, eventName, data) {
-    let channel = this.pusher.channelFor(channelName);
+  pusherTrigger(channelName, eventName, data, pusherInstance) {
+    let channel = pusherInstance ? pusherInstance.channelFor(channelName) : this.pusher.channelFor(channelName);
     channel.trigger(eventName, data);
   }
 });
